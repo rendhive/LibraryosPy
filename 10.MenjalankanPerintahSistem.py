a56195567afc08dae3279417789e056cbbb45af0
Rendhive code <rendhivecode@gmail.com>
2025-11-06 01:57:59 +0700

    Add files via upload

---
+++ 10.MenjalankanPerintahSistem.py
@@ -0,0 +1,7 @@
+import os
+
+# Menjalankan perintah sistem untuk menampilkan isi direktori
+os.system('ls')  # gunakan 'dir' untuk Windows
+
+
+#Menjalankan perintah sistem untuk menampilkan daftar file dan direktori dalam direktori saat ini.
